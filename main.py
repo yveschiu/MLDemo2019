f06ff57e7de7b8c740877ce3189f1b05c0fb25f2
yveschiu <yves.s.c.chiu@gmail.com>
2019-02-23 12:45:43 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+xs = [1,2,3,4,5]
+ys = [1,2,3,4,5]
+
+import matplotlib.pyplot as plt
+
+
+plt.title("")
+plt.xlabel("Numbers of machine")
+plt.ylabel("Capacity")
+plt.plot(xs, ys, "m")
+plt.show()
+
+李白,春思,燕草如碧絲，秦桑低綠枝。當君懷歸日，是妾斷腸時。春風不相識，何事入羅幃。
